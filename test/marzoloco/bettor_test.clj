1d2690bb792d1ca5dc5755da38290a34ba89a12f
Tony van Riet <tonyvanriet@gmail.com>
2016-01-26 23:56:09 -0600

    Clean up naming

--- test/marzoloco/bettor_test.clj
+++ test/marzoloco/bettor_test.clj
@@ -2,16 +2,16 @@
   (:require [clojure.test :refer :all]
             [marzoloco.aggregates.bettor :refer :all]))
 
-(deftest apply-bettor-funds-deposited-event
+(deftest apply-funds-deposited-event
   (let [bettor-id "betty"
         starting-bankroll 0.0M
         deposited-amount 200.0M
-        initial-agg (map->Bettor {:bettor-id bettor-id
-                                  :bankroll  starting-bankroll})
-        bfd-event {:event-type :bettor-funds-deposited
-                   :bettor-id  bettor-id
-                   :amount     deposited-amount}
-        expected-agg (map->Bettor {:bettor-id bettor-id
-                                   :bankroll  (+ starting-bankroll deposited-amount)})
-        actual-agg (apply-event initial-agg bfd-event)]
-    (is (= expected-agg actual-agg))))
+        initial-bettor (map->Bettor {:bettor-id bettor-id
+                                     :bankroll  starting-bankroll})
+        funds-deposited-event {:event-type :funds-deposited
+                               :bettor-id  bettor-id
+                               :amount     deposited-amount}
+        expected-bettor (map->Bettor {:bettor-id bettor-id
+                                      :bankroll  (+ starting-bankroll deposited-amount)})
+        actual-bettor (apply-event initial-bettor funds-deposited-event)]
+    (is (= expected-bettor actual-bettor))))
