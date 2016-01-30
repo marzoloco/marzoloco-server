df60787c5fa027704f0b6a6af883f7c386324ec8
Tony van Riet <tonyvanriet@gmail.com>
2016-01-30 16:29:48 -0600

    Remove placeholder test

--- src/marzoloco/core.clj
+++ src/marzoloco/core.clj
@@ -1,10 +1,6 @@
 (ns marzoloco.core
   (:gen-class))
 
-(defn seventy-one
-  []
-  72)
-
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/marzoloco/core_test.clj
+++ test/marzoloco/core_test.clj
@@ -1,7 +1,3 @@
 (ns marzoloco.core-test
   (:require [clojure.test :refer :all]
             [marzoloco.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= (seventy-one) 71))))
