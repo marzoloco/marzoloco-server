d20a21988cb22ec8b8cfaa2f935859b4dac464eb
Tony van Riet <tonyvanriet@gmail.com>
2016-02-01 00:11:53 -0600

    Destructure event in apply-event

--- src/marzoloco/wagering/player.clj
+++ src/marzoloco/wagering/player.clj
@@ -12,17 +12,17 @@
 
 (s/defmethod apply-event PointsDeposited
              [player :- Player
-              event :- PointsDeposited]
-             (update-in player [:bankroll] + (:amount event)))
+              {:keys [amount] :as event} :- PointsDeposited]
+             (update-in player [:bankroll] + amount))
 
 (s/defmethod apply-event WagerPlaced
              [player :- Player
-              event :- WagerPlaced]
+              {:keys [amount wager-id] :as event} :- WagerPlaced]
              (-> player
-                 (update-in [:bankroll] - (:amount event))
-                 (update-in [:open-wagers] conj (:wager-id event))))
+                 (update-in [:bankroll] - amount)
+                 (update-in [:open-wagers] conj wager-id)))
 
 (s/defmethod apply-event WinningsEarned
              [player :- Player
-              event :- WinningsEarned]
-             (update-in player [:winnings] + (:amount event)))
+              {:keys [amount] :as event} :- WinningsEarned]
+             (update-in player [:winnings] + amount))
