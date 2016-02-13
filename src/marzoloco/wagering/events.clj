85da4d78b243ef8fe9a063a40818a196f83e2308
Tony van Riet <tonyvanriet@gmail.com>
2016-02-12 23:21:01 -0600

    Refactor events and commands to defschema and use s/Num
    Using a lighter-weight approach to declaring the schema of the commands
    and events that happens to work better with ring-swagger.
    Using s/Num instead of BigDecimal since I can't easily pass a
    BigDecimal in JSON.

--- src/marzoloco/wagering/events.clj
+++ src/marzoloco/wagering/events.clj
@@ -1,37 +1,48 @@
 (ns marzoloco.wagering.events
   (:require [schema.core :as s]))
 
-(s/defrecord PointsDeposited [player-id :- s/Uuid
-                              amount :- BigDecimal])
+(s/defschema PointsDeposited {:event-type (s/eq :points-deposited)
+                              :player-id  s/Uuid
+                              :amount     s/Num})
 
-(s/defrecord WagerPlaced [player-id :- s/Uuid
-                          wager-id :- s/Uuid
-                          amount :- BigDecimal
-                          odds :- BigDecimal])
+(s/defschema WagerPlaced {:event-type (s/eq :wager-placed)
+                          :player-id  s/Uuid
+                          :wager-id   s/Uuid
+                          :amount     s/Num
+                          :odds       s/Num})
 
-(s/defrecord OverdrawAttempted [player-id :- s/Uuid
-                                wager-id :- s/Uuid])
+(s/defschema OverdrawAttempted {:event-type (s/eq :overdraw-attempted)
+                                :player-id  s/Uuid
+                                :wager-id   s/Uuid})
 
-(s/defrecord WagerWithdrawn [player-id :- s/Uuid
-                             wager-id :- s/Uuid])
+(s/defschema WagerWithdrawn {:event-type (s/eq :wager-withdrawn)
+                             :player-id  s/Uuid
+                             :wager-id   s/Uuid})
 
-(s/defrecord LockedWagerWithdrawAttempted [player-id :- s/Uuid
-                                           wager-id :- s/Uuid])
+(s/defschema LockedWagerWithdrawAttempted {:event-type (s/eq :locked-wager-withdraw-attempted)
+                                           :player-id  s/Uuid
+                                           :wager-id   s/Uuid})
 
-(s/defrecord WagerCancelled [player-id :- s/Uuid
-                             wager-id :- s/Uuid])
+(s/defschema WagerCancelled {:event-type (s/eq :wager-cancelled)
+                             :player-id  s/Uuid
+                             :wager-id   s/Uuid})
 
-(s/defrecord WagerLocked [player-id :- s/Uuid
-                          wager-id :- s/Uuid])
+(s/defschema WagerLocked {:event-type (s/eq :wager-locked)
+                          :player-id  s/Uuid
+                          :wager-id   s/Uuid})
 
-(s/defrecord WagerWon [player-id :- s/Uuid
-                       wager-id :- s/Uuid])
+(s/defschema WagerWon {:event-type (s/eq :wager-won)
+                       :player-id  s/Uuid
+                       :wager-id   s/Uuid})
 
-(s/defrecord WagerPushed [player-id :- s/Uuid
-                          wager-id :- s/Uuid])
+(s/defschema WagerPushed {:event-type (s/eq :wager-pushed)
+                          :player-id  s/Uuid
+                          :wager-id   s/Uuid})
 
-(s/defrecord WagerLost [player-id :- s/Uuid
-                        wager-id :- s/Uuid])
+(s/defschema WagerLost {:event-type (s/eq :wager-lost)
+                        :player-id  s/Uuid
+                        :wager-id   s/Uuid})
 
-(s/defrecord WinningsEarned [player-id :- s/Uuid
-                             amount :- BigDecimal])
+(s/defschema WinningsEarned {:event-type (s/eq :winnings-earned)
+                             :player-id  s/Uuid
+                             :amount     s/Num})
